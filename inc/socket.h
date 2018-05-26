ac38fbca5ce7f575e8ce721fd2d92e9430f1ca4c
Scott Heidbrink <scott.heidbrink@gmail.com>
2018-05-25 23:07:19 -0600

    Added timeout ability in tcp_connect_to Changed how sock_create so it can be more general reverted the Makefile because it was causing obj files to always be recreated because the bin folder was new?

--- inc/socket.h
+++ inc/socket.h
@@ -5,7 +5,15 @@
  * all_ips must be freed if return int != -1
  */
 int tcp_connect(char* host, char* port);
+int tcp_connect_to(char* host, char* port, int sec, int usec);
 int udp_connect(char* host, char* port);
-int sock_connect(char* host, char* port, int socktype, int proto);
+int sock_create(char* host,
+                char* port,
+                int socktype,
+                int proto,
+                int (*handle_func)(int, const struct sockaddr*, socklen_t));
 int cidr_to_in_addr(char* cidr, struct in_addr** all_ips, uint32_t* mask);
+int setblocking(int sock, int blocking);
+
+
 #endif
--- src/socket.c
+++ src/socket.c
@@ -1,12 +1,16 @@
 #include "socket.h"
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+
+struct timeval tv;
 
 int cidr_to_in_addr(char* cidr, struct in_addr** all_ips, uint32_t* mask) {
     uint8_t first, second, third, fourth, range;
@@ -30,15 +34,61 @@ int cidr_to_in_addr(char* cidr, struct in_addr** all_ips, uint32_t* mask) {
     return len;
 }
 
+int fast_connect(int sfd, const struct sockaddr* addr, socklen_t addrlen) {
+    fd_set fdset;
+
+    setblocking(sfd, 0);
+    connect(sfd, addr, addrlen);
+
+    FD_ZERO(&fdset);
+    FD_SET(sfd, &fdset);
+
+    if (select(sfd + 1, NULL, &fdset, NULL, &tv) == 1) {
+        int so_error;
+        socklen_t len = sizeof(so_error);
+        getsockopt(sfd, SOL_SOCKET, SO_ERROR, &so_error, &len);
+        return so_error;
+    }
+    return -1;
+}
+
 int tcp_connect(char* host, char* port){
-    return sock_connect(host, port, SOCK_STREAM, IPPROTO_TCP);
+    return sock_create(host, port, SOCK_STREAM, IPPROTO_TCP, connect);
+}
+
+int tcp_connect_to(char* host, char* port, int sec, int usec){
+    extern struct timeval tv;
+    tv.tv_sec = sec;
+    tv.tv_usec = usec;
+    return sock_create(host, port, SOCK_STREAM, IPPROTO_TCP, fast_connect);
 }
 
 int udp_connect(char* host, char* port){
-    return sock_connect(host, port, SOCK_DGRAM, IPPROTO_UDP);
+    return sock_create(host, port, SOCK_DGRAM, IPPROTO_UDP, connect);
 }
 
-int sock_connect(char* host, char* port, int socktype, int proto) {
+int setblocking(int sock, int blocking) {
+    int flags;
+    if ((flags = fcntl(sock, F_GETFL, 0)) == -1) {
+        return -1;
+    }
+    if (blocking)
+        flags &= ~O_NONBLOCK;
+    else
+        flags |= O_NONBLOCK;
+    if (fcntl(sock, F_SETFL, flags) == -1) {
+        return -1;
+    }
+    return 0;
+}
+
+int sock_create(
+        char* host, 
+        char* port, 
+        int socktype, 
+        int proto, 
+        int (*handle_func)(int, const struct sockaddr*, socklen_t)) {
+
     struct addrinfo hints;
     struct addrinfo *result, *rp;
     int rint, sockfd;
@@ -58,8 +108,10 @@ int sock_connect(char* host, char* port, int socktype, int proto) {
         sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
         if (sockfd == -1)
             continue;
-        if (connect(sockfd, rp->ai_addr, rp->ai_addrlen) != -1)
+
+        if (handle_func == NULL || handle_func(sockfd, rp->ai_addr, rp->ai_addrlen) == 0)
             break;
+
         close(sockfd);
     }
 
